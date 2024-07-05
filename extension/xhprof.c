857e385a151dfb06ec11a954ce518fa29049f2f8
Remi Collet <remi@remirepo.net>
2024-07-05 15:29:22 +0200

    Fix php_pcre_match_impl call for 8.4

--- extension/xhprof.c
+++ extension/xhprof.c
@@ -1286,10 +1286,15 @@ int hp_pcre_match(zend_string *pattern, const char *str, size_t len, zend_ulong 
 #if PHP_VERSION_ID < 70400
         php_pcre_match_impl(pce_regexp, (char*)str, len, &matches, &subparts /* subpats */,
                         0/* global */, 0/* ZEND_NUM_ARGS() >= 4 */, 0/*flags PREG_OFFSET_CAPTURE*/, 0/* start_offset */);
-#else
+#elif PHP_VERSION_ID < 80400
         zend_string *tmp = zend_string_init(str, len, 0);
         php_pcre_match_impl(pce_regexp, tmp, &matches, &subparts /* subpats */,
                             0/* global */, 0/* ZEND_NUM_ARGS() >= 4 */, 0/*flags PREG_OFFSET_CAPTURE*/, 0/* start_offset */);
+        zend_string_release(tmp);
+#else
+        zend_string *tmp = zend_string_init(str, len, 0);
+        php_pcre_match_impl(pce_regexp, tmp, &matches, &subparts /* subpats */,
+                            false/* global */, 0/*flags PREG_OFFSET_CAPTURE*/, 0/* start_offset */);
         zend_string_release(tmp);
 #endif
 
